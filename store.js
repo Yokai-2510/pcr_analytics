cc0c6699c07ed7c396a00ab5d89f213ee97047c2
Yokai-2510 <ethanarkham@gmail.com>
2026-05-11 19:24:24 +0530

    Update API base to HTTPS DuckDNS

--- store.js
+++ store.js
@@ -2,7 +2,7 @@
 const subs = new Set();
 
 const initial = {
-  apiBase: localStorage.getItem('apiBase') || 'http://3.111.218.114:8000',
+  apiBase: localStorage.getItem('apiBase') || 'https://pcr-analytics.duckdns.org',
   adminToken: localStorage.getItem('adminToken') || '',
   isLoggedIn: !!localStorage.getItem('adminToken'),
   connected: false,
